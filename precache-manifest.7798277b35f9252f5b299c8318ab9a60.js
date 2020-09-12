830d9f591a2299682b2b66541f363559544cf1c9
rohit pandey <1080pandey@gmail.com>
2020-09-12 22:12:25 +0530

    Updates

---
+++ precache-manifest.7798277b35f9252f5b299c8318ab9a60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bdf73ce34228bdbb462d0a5b386be61",
+    "url": "/covid19LiveUpdate/index.html"
+  },
+  {
+    "revision": "4ba76ad06ef954e22fb8",
+    "url": "/covid19LiveUpdate/static/css/main.a31ea725.chunk.css"
+  },
+  {
+    "revision": "fde915c2bee33926d46f",
+    "url": "/covid19LiveUpdate/static/js/2.87fa1350.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19LiveUpdate/static/js/2.87fa1350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ba76ad06ef954e22fb8",
+    "url": "/covid19LiveUpdate/static/js/main.2dcb00d1.chunk.js"
+  },
+  {
+    "revision": "855af853d313d2a0ab4f",
+    "url": "/covid19LiveUpdate/static/js/runtime-main.638b0b1d.js"
+  },
+  {
+    "revision": "cc494aef9d712dc00ea38f94d08c7127",
+    "url": "/covid19LiveUpdate/static/media/covid.cc494aef.png"
+  }
+]);
